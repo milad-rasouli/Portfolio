06b430303ec1a97f0908a0abf161c4358c671b73
Milad Rasouli <milad75rasouli@gmail.com>
2024-04-25 11:39:14 +0330

    chore: verify jwt token

--- internal/jwt/jwt.go
+++ internal/jwt/jwt.go
@@ -1,11 +1,18 @@
 package jwt
 
 import (
+	"errors"
+	"strings"
 	"time"
 
-	"github.com/Milad75Rasouli/portfolio/internal/model"
 	"github.com/golang-jwt/jwt/v5"
 )
+
+var InvalidJWTMethod = errors.New("Invalid JWT Method")
+var FailedToParseToken = errors.New("Failed to parse the token")
+var InvalidToken = errors.New("Invalid token")
+var InvalidTokenClaims = errors.New("Invalid token claims")
+var FailedToReadClaims = errors.New("Failed to read the claims")
 
 type JWT struct {
 	cfg Config
@@ -15,17 +22,23 @@ func NewJWT(c Config) *JWT {
 	return &JWT{cfg: c}
 }
 
-func (j JWT) createToken(user model.User) (string, error) {
+type JWTUser struct {
+	FullName string
+	Email    string
+	Role     string
+}
+
+func (j JWT) CreateToken(jwtUser JWTUser) (string, error) {
 
 	now := time.Now()
 	claims := &jwt.RegisteredClaims{
 		Issuer:    "MiladRasouli.ir",
-		Subject:   user.FullName,
-		Audience:  []string{"admin"},                      //TODO: change based on their individual role
-		ExpiresAt: jwt.NewNumericDate(now.Add(time.Hour)), //TODO: choose a reasonable amount
+		Subject:   jwtUser.FullName,
+		Audience:  []string{jwtUser.Role},
+		ExpiresAt: jwt.NewNumericDate(now.Add(time.Second)), //TODO: choose a reasonable amount
 		NotBefore: jwt.NewNumericDate(now),
 		IssuedAt:  jwt.NewNumericDate(now),
-		ID:        user.Email,
+		ID:        jwtUser.Email,
 	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
@@ -35,4 +48,42 @@ func (j JWT) createToken(user model.User) (string, error) {
 	}
 
 	return tokenString, nil
+}
+
+func (j JWT) VerifyParseUserToken(tokenString string) (JWTUser, error) {
+	var jwtUser JWTUser
+
+	token, err := jwt.ParseWithClaims(tokenString, new(jwt.RegisteredClaims),
+		func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, InvalidJWTMethod
+			}
+
+			return []byte(j.cfg.SecretKey), nil
+		})
+
+	if err != nil {
+		return jwtUser, errors.Join(FailedToParseToken, err)
+	}
+	if token.Valid == false {
+		return jwtUser, InvalidToken
+	}
+	claims, ok := token.Claims.(*jwt.RegisteredClaims)
+	if ok == false {
+		return jwtUser, InvalidTokenClaims
+	}
+
+	jwtUser.Email = claims.ID
+	s, err := claims.GetSubject()
+	if err != nil {
+		return jwtUser, errors.Join(FailedToReadClaims, errors.New("subject claims error"))
+	}
+	jwtUser.FullName = s
+	a, err := claims.GetAudience()
+	if err != nil {
+		return jwtUser, errors.Join(FailedToReadClaims, errors.New("audience claims error"))
+	}
+	jwtUser.Role = strings.Join(a, "")
+
+	return jwtUser, nil
 }
--- internal/jwt/jwt_test.go
+++ internal/jwt/jwt_test.go
@@ -3,30 +3,43 @@ package jwt
 import (
 	"fmt"
 	"testing"
-	"time"
 
-	"github.com/Milad75Rasouli/portfolio/internal/model"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestTokenStuff(t *testing.T) {
+	var (
+		token string
+		err   error
+	)
 	cfg := Config{
 		SecretKey: "VerySecretKey",
 	}
-	now := time.Now()
-	user := model.User{
-		ID:         1,
-		FullName:   "foo bar",
-		Email:      "bar@baz.com",
-		Password:   "foobarbaz",
-		IsGithub:   0,
-		CreatedAt:  now,
-		ModifiedAt: now,
-		OnlineAt:   now,
+	user := JWTUser{
+		FullName: "foo bar",
+		Email:    "bar@baz.com",
+		Role:     "admin",
+	}
+	jwt := NewJWT(cfg)
+
+	{
+		token, err = jwt.CreateToken(user)
+		assert.NoError(t, err)
+		fmt.Printf("created token: %s\n", token)
 	}
 
-	jwt := NewJWT(cfg)
-	token, err := jwt.createToken(user)
-	assert.NoError(t, err)
-	fmt.Printf("created token: %s", token)
+	{
+		jwtUser, err := jwt.VerifyParseUserToken(token)
+		assert.NoError(t, err)
+		fmt.Printf("parsed token: %+v\n", jwtUser)
+	}
+
+	{
+		expiredToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJNaWxhZFJhc291bGkuaXIiLCJzdWIiOiJmb28gYmFyIiwiYXVkIjpbImFkbWluIl0sImV4cCI6MTcxNDAzMTM5NCwibmJmIjoxNzE0MDMxMzkzLCJpYXQiOjE3MTQwMzEzOTMsImp0aSI6ImJhckBiYXouY29tIn0.7UVbHcnh3w70TZAywpexFhc3az_S77hYHcUezM19xtM"
+		jwtUser, err := jwt.VerifyParseUserToken(expiredToken)
+		assert.Error(t, err)
+
+		fmt.Printf("parsed token: %+v expiration error: %+v\n", jwtUser, err)
+	}
+
 }
